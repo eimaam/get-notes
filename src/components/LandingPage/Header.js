71d776b6eb28414a651509d671339ea1518c1bf5
eimaam <imamddahir@gmail.com>
2022-10-11 21:29:55 +0100

    added netlify.tmol setting for redirects

--- src/components/LandingPage/Header.js
+++ src/components/LandingPage/Header.js
@@ -33,7 +33,7 @@ export default function Header(){
             <span>&nbsp;</span>
             <span>&nbsp;</span>
             <button>
-              <a href='/signup'> get started</a>
+              <a href='/login'> get started</a>
             </button>
         </div>
             <img src={heroImage2} alt="web designer" data-aos="fade-right"/>
