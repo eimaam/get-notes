03ee2ba692e37e3ea112bd6d70e7dba10aa2f78c
eimaam <imamddahir@gmail.com>
2022-11-15 18:55:33 +0100

    switched quotes fetching from api to local

--- src/components/LandingPage/DailyQuotes.js
+++ src/components/LandingPage/DailyQuotes.js
@@ -2,23 +2,14 @@ import React, { useState } from 'react'
 import { useEffect } from 'react';
 import { PuffLoader } from 'react-spinners';
 import { useAuth } from '../../contexts/AuthContext';
+import { quotesData } from './quotesData';
 
 export const DailyQuotes = () => {
-  const {loading, setLoading} = useAuth()
-    const random = Math.floor(Math.random() * (1643-1) - 1);
-    const [data, setData] = useState([])
+  const random = Math.floor(Math.random() * (247-1) - 1)
+
     
 
-      const getQuote = async () => {
-        const data = await fetch('https://type.fit/api/quotes/')
-        .then(res => res.json())
-        .then(data => setData(prevState => data))
-        setLoading(false)
-      }
-
-      useEffect(() => {
-        getQuote()
-      }, [])
+   
 
   return (
     <div className='quotes--section' data-aos="fade-up">
@@ -26,27 +17,24 @@ export const DailyQuotes = () => {
             <h2>DAILY QUOTES</h2>
             <hr />
         </div>
-        {loading ? <PuffLoader /> :
         <div id='quotes'>
             <div className='quote--card' data-aos="zoom-up">
-                <p>"{data[random].text}"</p>
-                <i>{data[random].author == "" ? "ANON" : data[random].author}</i>
+              <p>"{quotesData[random].text}"</p>
+              <i>{quotesData[random].author == "" ? "ANON" : quotesData[random].author}</i>
             </div>
             <div className='quote--card' data-aos="zoom-up">
-                <p>"{data[random+1].text}"</p>
-                <i>{data[random+1].author == "" ? "ANON" : data[random+1].author}</i>
+              <p>"{quotesData[random+1].text}"</p>
+              <i>{quotesData[random+1].author == "" ? "ANON" : quotesData[random+1].author}</i>
             </div>
             <div className='quote--card' data-aos="zoom-up">
-                <p>"{data[random+2].text}"</p>
-                <i>{data[random+2].author == "" ? "ANON" : data[random+2].author}</i>
+              <p>"{quotesData[random+2].text}"</p>
+              <i>{quotesData[random+2].author == "" ? "ANON" : quotesData[random+2].author}</i>
             </div>
         </div>
-        }
         <div style={{margin:"auto", textAlign: "center"}}>
             <p><i>Light your day with more quotes from our super App:</i></p>
             <button><a href="https://getquotes.eimaam.dev" target="_blank">getQUOTES</a></button>
         </div>
-
     </div>
   )
 }
